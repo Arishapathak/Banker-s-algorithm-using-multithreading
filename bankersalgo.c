65d5fc344d9dd72232d49a55a4cd90f86a2bf6b2
Arishapathak <55892709+Arishapathak@users.noreply.github.com>
2020-03-24 17:00:45 +0530

    Update bankersalgo.c

--- bankersalgo.c
+++ bankersalgo.c
@@ -7,11 +7,12 @@ main()
 	scanf("%d",&P);
 	printf("Enter the number of total number of resources");
 	scanf("%d",&R);
-	int maximum[10][10],need[10][10],allocation[10][10],available[10],safesequence[10],completed[10];
+	int maximum[P][R],need[P][R],allocation[P][R],available[R],safesequence[P],completed[P];
 	for(int i=0;i<P;i++)
 	{
 		completed[i]=0;
 	}
+        //MAXIMUM LIMIT
 	printf("Enter the maximum limit");
 	for(int i=0;i<P;i++)
 	{
@@ -21,6 +22,7 @@ main()
 			scanf("%d",&maximum[i][j]);
 		}
 	}
+        //ALLOCATION OF RESOURCES
 	printf("Enter the allocation of each process");
 	for(int i=0;i<P;i++)
 	{
@@ -30,6 +32,7 @@ main()
 			scanf("%d",&allocation[i][j]);
 		}
 	}
+        //AVAILABLE RESOURCES
 	printf("Enter the available resources");
 	for(int i=0;i<R;i++)
 	{
